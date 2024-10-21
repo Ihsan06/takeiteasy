aef6389650411caa28bccf21c6da4cbfca66b2f4
IhsanIYilmaz <ihsan.i.yilmaz@deutschebahn.com>
2024-10-22 01:37:35 +0200

    success end day 2

--- video.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import *
-
-def create_video_with_quote(quote, output_path):
-    clip = TextClip(quote, fontsize=70, color='white', size=(720, 480))
-    clip = clip.set_duration(10).set_position('center').set_bg_color('black')
-
-    clip.write_videofile(output_path, fps=24)
