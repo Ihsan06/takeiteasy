e5b04159f1266aa1be958d2965bd257a5245fe72
IhsanIYilmaz <ihsan.i.yilmaz@deutschebahn.com>
2024-10-22 01:36:41 +0200

    success end day 2

--- main.py
+++ main.py
@@ -52,19 +52,19 @@ audio_file_path = 'audio/idea10.mp3'  # Beispielpfad zur Audiodatei
 
 def generate_quote():
     """Generiert ein inspirierendes Zitat mit der OpenAI API."""
-    log_with_color("\nGeneriere Zitat mit OpenAI API...\n", Fore.CYAN, 1.5)
+    log_with_color("\n Generiere Zitat mit OpenAI API...\n", Fore.CYAN, 1.5)
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt="Generate an inspirational quote",
         max_tokens=50
     )
     quote = response.choices[0].text.strip()
-    log_with_color(f"\nGeneriertes Zitat: {quote}\n", Fore.GREEN, 1.5)
+    log_with_color(f"\n Generiertes Zitat: {quote}\n", Fore.GREEN, 1.5)
     return quote
 
 def save_quote_to_csv(quote, csv_file):
     """Speichert das Zitat in einer CSV-Datei."""
-    log_with_color(f"\nSpeichere Zitat in {csv_file} ...\n", Fore.YELLOW, 1.5)
+    log_with_color(f"\n Speichere Zitat in {csv_file} ...\n", Fore.YELLOW, 1.5)
     with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([quote])
@@ -72,11 +72,11 @@ def save_quote_to_csv(quote, csv_file):
 
 def get_quotes_from_csv(csv_file):
     """Liest alle Zitate aus der CSV-Datei und gibt sie als Liste zurück."""
-    log_with_color(f"\n [1/7]] Lade Zitate aus {csv_file} ...\n", Fore.RED, 1.5)
+    log_with_color(f"\n [1/7] Lade Zitate aus {csv_file} ...\n", Fore.BLUE, 1.5)
     with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
         reader = csv.reader(file)
         quotes = [row[0] for row in reader]
-    log_with_color(f"\nEs wurden {len(quotes)} Zitate aus der CSV-Datei geladen.\n", Fore.MAGENTA, 1.5)
+    log_with_color(f"\nEs wurden {len(quotes)} Zitate aus der CSV-Datei geladen.\n", Fore.BLUE, 1.5)
     return quotes
 
 
@@ -87,37 +87,42 @@ def get_quotes_from_csv(csv_file):
 ##########################################################################################################
 
 
-def resize_image(image_path):
-    """Öffne ein Bild mit Pillow und ändere die Größe vor der Übergabe an MoviePy."""
-    log_with_color(f"\n [3/7]] Konvertierung des Bildes von .webp zu .png ...\n", Fore.BLUE, 1.0)
+def convert_image_to_png(image_path):
+    """Konvertiert ein Bild von .webp oder anderen Formaten zu .png."""
+    log_with_color(f"\n [3/7] Konvertierung des Bildes von .webp zu .png ...\n", Fore.RED, 1.0)
+    
     img = Image.open(image_path)
-    resized_image_path = image_path.replace('.webp', '.png')  # Für WebP-Konvertierung oder andere Formate
-    img.save(resized_image_path)
-    log_with_color(f"\nBild erfolgreich konvertiert und gespeichert unter: {resized_image_path}\n", Fore.GREEN, 1.0)
-    return resized_image_path
+    converted_image_path = image_path.replace('.webp', '.png')  # Für WebP-Konvertierung
+    img.save(converted_image_path)
+    
+    log_with_color(f"\n Bild erfolgreich konvertiert und gespeichert unter: {converted_image_path}\n", Fore.RED, 1.0)
+    
+    return converted_image_path
 
 def get_random_background_image():
     """Wählt ein zufälliges Bild aus dem Hintergrundverzeichnis aus und konvertiert .webp falls nötig."""
-    log_with_color(f"\n [2/7]] Wähle zufälliges Hintergrundbild aus dem Verzeichnis {image_directory} ...\n", Fore.CYAN, 1.0)
+    log_with_color(f"\n [2/7] Wähle zufälliges Hintergrundbild aus dem Verzeichnis {image_directory} ...\n", Fore.MAGENTA, 1.0)
     
     if not os.path.exists(image_directory):
-        log_with_color(f"\nHintergrundverzeichnis {image_directory} nicht gefunden.\n", Fore.RED, 1.5)
+        log_with_color(f"\n Hintergrundverzeichnis {image_directory} nicht gefunden.\n", Fore.RED, 1.5)
         return None, None
 
     images = [img for img in os.listdir(image_directory) if img.endswith(('png', 'jpg', 'jpeg', 'webp'))]
     if not images:
-        log_with_color(f"\nKeine Hintergrundbilder im Verzeichnis {image_directory} gefunden.\n", Fore.RED, 1.5)
+        log_with_color(f"\n Keine Hintergrundbilder im Verzeichnis {image_directory} gefunden.\n", Fore.RED, 1.5)
         return None, None
 
     chosen_image = os.path.join(image_directory, random.choice(images))
-    log_with_color(f"\nAusgewähltes Bild: {chosen_image}\n", Fore.YELLOW, 1.0)
+    log_with_color(f"\n Ausgewähltes Bild: {chosen_image}\n", Fore.MAGENTA, 1.0)
 
     # Extrahiere den Bildnamen ohne Pfad und Erweiterung
     image_name = os.path.splitext(os.path.basename(chosen_image))[0]
 
-    # Bildgröße anpassen und konvertieren, falls nötig
-    resized_image_path = resize_image(chosen_image)
-    return resized_image_path, image_name
+    # Konvertiere zu PNG, falls nötig
+    if chosen_image.endswith('.webp'):
+        chosen_image = convert_image_to_png(chosen_image)
+
+    return chosen_image, image_name
 
 
 ##########################################################################################################
@@ -127,18 +132,17 @@ def get_random_background_image():
 ##########################################################################################################
 
 
-def create_video_with_quote(quote, output_path, resized_image_path, image_name):
+def create_video_with_quote(quote, output_path, image_path, image_name):
     """Erstellt ein Video mit dem angegebenen Zitat und einer Audiodatei."""
-    log_with_color(f"\n [4/7]]Erstelle Video für Zitat: {quote}\n", Fore.CYAN, 1.5)
+    log_with_color(f"\n [4/7] Erstelle Video für Zitat: {quote}\n", Fore.YELLOW, 1.5)
 
     # Verwende das übergebene Hintergrundbild und den Bildnamen
-    
-    if resized_image_path is None:
+    if image_path is None:
         log_with_color("\nKein Hintergrundbild gefunden. Video kann nicht erstellt werden.\n", Fore.RED, 1.5)
         return
 
     # Erstelle den ImageClip
-    image_clip = ImageClip(resized_image_path).set_duration(10)
+    image_clip = ImageClip(image_path).set_duration(10)
 
 
 ########################################       BOX Section      ########################################
@@ -176,6 +180,7 @@ def create_video_with_quote(quote, output_path, resized_image_path, image_name):
     # Kombiniere den Hintergrund, die Box und den Text
     video = CompositeVideoClip([image_clip, box_clip, text_clip])
 
+    log_with_color(f"\n Video erstellt für Zitat: {quote}\n", Fore.YELLOW, 1.5)
 
 ########################################       AUDIO Section      ########################################
 
@@ -186,13 +191,15 @@ def create_video_with_quote(quote, output_path, resized_image_path, image_name):
         audio_clip = AudioFileClip(audio_file_path).set_duration(video.duration)
         # Füge die Audiodatei zum Video hinzu
         video = video.set_audio(audio_clip)
-        log_with_color(f"\nAudiodatei {audio_file_path} erfolgreich hinzugefügt.\n", Fore.GREEN, 1.5)
+        log_with_color(f"\n Audiodatei {audio_file_path} erfolgreich hinzugefügt.\n", Fore.CYAN, 1.5)
     else:
-        log_with_color(f"\nAudiodatei {audio_file_path} nicht gefunden.\n", Fore.RED, 1.5)
+        log_with_color(f"\n Audiodatei {audio_file_path} nicht gefunden.\n", Fore.RED, 1.5)
 
-    log_with_color(f"\n [6/7]]Speichere Video zu {output_path} ...\n", Fore.YELLOW, 1.5)
+    log_with_color(f"\n [6/7] Speichere Video zu {output_path} ...\n", Fore.GREEN, 1.5)
+    
     video.write_videofile(output_path, fps=24)
-    log_with_color(f"\nVideo erfolgreich erstellt: {output_path}\n", Fore.GREEN, 1.5)
+    
+    log_with_color(f"\n Video erfolgreich erstellt: {output_path}\n", Fore.GREEN, 1.5)
 
 
 ##########################################################################################################
@@ -212,16 +219,16 @@ def main():
     if quotes:
         for idx, quote in enumerate(quotes):
             # Hintergrundbild zufällig auswählen
-            resized_image_path, image_name = get_random_background_image()
-            if resized_image_path and image_name:
+            image_path, image_name = get_random_background_image()
+            if image_path and image_name:
                 # Erstelle einen Zeitstempel für den Videonamen
                 timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
                 output_path = f'{video_directory}/video_{timestamp}_{image_name}_{idx + 1}.mp4'
-                create_video_with_quote(quote, output_path, resized_image_path, image_name)
+                create_video_with_quote(quote, output_path, image_path, image_name)
     else:
-        log_with_color("\nKeine Zitate in der CSV gefunden.\n", Fore.RED, 1.5)
+        log_with_color("\n Keine Zitate in der CSV gefunden.\n", Fore.RED, 1.5)
     
-    log_with_color("\n\n [7/7]]Hauptprozess abgeschlossen.\n\n", Fore.GREEN, 1.5)
+    log_with_color("\n\n [7/7] Hauptprozess abgeschlossen.\n\n", Fore.GREEN, 1.5)
 
 if __name__ == "__main__":
     main()
